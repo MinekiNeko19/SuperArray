b21bea100e23827326879a96becaa75ea6b6cfd1
Sarah Leong <sleong20@stuy.edu>
2020-11-03 07:59:04 -0500

    Added and tested constructor and size method

---
+++ SuperArray.java
@@ -0,0 +1,33 @@
+public class SuperArray {
+    private String[] data;
+    private int size; // the current size
+
+    // constructor
+    public SuperArray() {
+        data = new String[10];
+        size = 0;
+    }
+
+    // methods
+    public int size() {
+        return size;
+    }
+
+    // public boolean add(String str) {
+    //     data[size] = str;
+    //     size++;
+    // }
+
+    // public String get(int index) {
+    //     return data[index];
+    // }
+
+    // public String set(int index, String element) {
+
+    // }
+
+    public static void main(String[] args) {
+        SuperArray fruits = new SuperArray();
+        System.out.println(fruits.size());
+    }
+}
