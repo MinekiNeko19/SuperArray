c7ca16b87bbc16e1584556416e3c06d13527d9c8
Sarah Leong <sleong20@stuy.edu>
2020-11-04 13:04:46 -0500

    changed capacity increase in resize

--- SuperArray.java
+++ SuperArray.java
@@ -33,7 +33,7 @@ public class SuperArray {
     }
 
     private void resize() {
-        String[] bigger = new String[data.length+2];
+        String[] bigger = new String[data.length+(data.length*2)];
         for (int i = 0; i < data.length; i++) {
             bigger[i]=data[i];
         }
@@ -67,23 +67,27 @@ public class SuperArray {
         return list;
     }
 
+    public SuperArray(int initialCapacity) {
+        data = new String[initialCapacity];
+        size = 0;
+    }
     public static void main(String[] args) {
         SuperArray fruits = new SuperArray();
         System.out.println(fruits.size());
-        System.out.println(fruits.add("apples"));
-        System.out.println(fruits.size());
-        System.out.println(fruits.get(0));
-        System.out.println(fruits.set(0,"oranges"));
-        System.out.println(fruits.add("apples"));
-        System.out.println(fruits.add("dragonfruit"));
-        System.out.println(fruits.add("grape"));
-        System.out.println(fruits.add("persimmon"));
-        System.out.println(fruits.add("pear"));
-        System.out.println(fruits.add("mango"));
-        System.out.println(fruits.add("tomato"));
-        System.out.println(fruits.add("banana"));
-        System.out.println(fruits.add("watermelon"));
-        System.out.println(fruits.add("pineapple"));
+        fruits.add("apples");
+        fruits.size();
+        fruits.get(0);
+        fruits.set(0,"oranges");
+        fruits.add("apples");
+        fruits.add("dragonfruit");
+        fruits.add("grape");
+        fruits.add("persimmon");
+        fruits.add("pear");
+        fruits.add("mango");
+        fruits.add("tomato");
+        fruits.add("banana");
+        fruits.add("watermelon");
+        fruits.add("pineapple");
         for (int i = 0; i < fruits.size();i++) {
             System.out.println(fruits.get(i));
         }
@@ -95,8 +99,8 @@ public class SuperArray {
 
         System.out.println(fruits.toString());
 
-        System.out.println(fruits.contains("banana"));
-        System.out.println(fruits.contains("turkey"));
-        System.out.println(words.contains("apple"));
+        // System.out.println(fruits.contains("banana"));
+        // System.out.println(fruits.contains("turkey"));
+        // System.out.println(words.contains("apple"));
     }
 }
